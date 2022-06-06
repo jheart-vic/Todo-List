462bcc05f4966096940243e7da1ac3d61c11c5a2
jheart-vic <adebowalevictorjheart.com>
2022-06-06 13:28:15 +0100

    Add new features

--- modules/task.js
+++ modules/task.js
@@ -1,26 +1,98 @@
 export default class Task {
-  constructor () {
-    this.lists = [];
+  constructor() {
+    this.lists = JSON.parse(localStorage.getItem('list')) || [];
+    this.editId = null;
   }
 
-  addList(description, completed, index) {
-    this.lists.push ({description, completed, index});
-    this.showLists();
-    this.lists.length = 0;
-  }
-  showLists () {
-    const wrapper = document.getElementById ('list-ul');
-    for (let index of this.lists) {
-      wrapper.innerHTML += `<li class="list-li">
-        <div class="list-input">
-        <div>
-        <input type="checkbox">
-        <label>${index.description}</label>
-        </div>
-        <span class="v-dots"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></span>
-        </div>
-        </li>
-        <hr>`;
+  addList() {
+    const taskInput = document.getElementById('task-input');
+    const list = { description: '', completed: false, index: 0 };
+    list.description = taskInput.value;
+    if (!list.description) {
+      return null;
+    } if (this.editId) {
+      const index = this.editId[this.editId.length - 1];
+      this.lists.splice(index, 1, list);
+      localStorage.setItem('list', JSON.stringify(this.lists));
+    } else {
+      list.index = this.lists.length + 1;
+      this.lists.push(list);
+      localStorage.setItem('list', JSON.stringify(this.lists));
+      this.showLists([list]);
+      taskInput.value = '';
     }
+    window.location.reload();
+    this.isEditing = null;
+    return true;
+  }
+
+  showLists(list) {
+    const renderedList = list || this.lists;
+    renderedList.forEach((item, index) => {
+      const wrapper = document.getElementById('list-ul');
+      wrapper.innerHTML += `<li class="list-li" id="collection-${index}">
+      <div class="list-input">
+      <div class="input-box">
+      <input type="checkbox" class="completed" required ${
+  item.completed ? 'checked' : ''
+}>
+      <label class="check ${
+  item.completed ? 'checked' : ''
+}" id="description-${index}">${item.description}</label>
+      </div>
+      <span><i class="fa fa-trash-o del-btn btn-${index}" aria-hidden="true"></i></i></span>
+      </div>
+      </li>`;
+    });
+  }
+
+  editTask(id, description) {
+    const input = document.getElementById('task-input');
+    input.value = description;
+    this.editId = id;
+  }
+
+  delete(deleteClass) {
+    const delList = document.querySelectorAll('.del-btn');
+    delList.forEach((item) => {
+      if (item.classList.contains(deleteClass)) {
+        const index = deleteClass[deleteClass.length];
+        this.lists.splice(index, 1);
+        this.lists.forEach((value, index) => {
+          value.index = index + 1;
+        });
+        localStorage.setItem('list', JSON.stringify(this.lists));
+        item.parentElement.parentElement.parentElement.remove();
+      }
+    });
+  }
+
+  completed() {
+    const listId = this.parentElement.parentElement.parentElement.id;
+    const index = listId[listId.length - 1];
+    const isElementChecked = this.checked;
+    this.nextElementSibling.classList.toggle('checked');
+    const lists = JSON.parse(localStorage.getItem('list'));
+    lists.splice(index, 1, {
+      description: this.nextElementSibling.innerHTML,
+      completed: isElementChecked,
+      index,
+    });
+    localStorage.setItem('list', JSON.stringify(lists));
+  }
+
+  clearComplete() {
+    const completedTasks = document.querySelectorAll('.checked');
+    completedTasks.forEach((task) => {
+      const { parentElement } = task.parentElement.parentElement;
+      const elementId = parentElement.id;
+      const index = elementId[elementId.length - 1];
+      this.lists[index].completed = true;
+      const filteredList = this.lists.filter((task) => task.completed !== true);
+      localStorage.setItem('list', JSON.stringify(filteredList));
+      window.location.reload();
+    });
   }
 }
+
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import './style.css';
 import Task from '../modules/task.js';
 
-const task = new Task ();
- task.addList( 'Dance with Sanja', true,  0)
- task.addList( 'Dance with Toma', false,  1)
- task.addList( 'Dance with Waleed', true,  )
-// task.addList({description: 'Dance with Sanja', completed: true, index: 1});
+const task = new Task();
+window.addEventListener('load', task.showLists());
+
+const btn = document.querySelectorAll('.del-btn');
+const clearBtn = document.querySelector('#btn');
+const listWrapper = document.querySelectorAll('.check');
+const addTaskInput = document.getElementById('task-input');
+const completed = document.querySelectorAll('.completed');
+const addBtn = document.getElementById('add-btn');
+
+addBtn.addEventListener('click', () => task.addList());
+
+addTaskInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    task.addList();
+  }
+});
+
+completed.forEach((item) => {
+  item.addEventListener('click', task.completed);
+});
+clearBtn.addEventListener('click', () => task.clearComplete());
+listWrapper.forEach((item) => {
+  item.addEventListener('click', () => {
+    task.editTask(item.id, item.innerText);
+  });
+});
+btn.forEach((item) => item.addEventListener('click', () => task.delete(item.classList[item.classList.length - 1])));
+
